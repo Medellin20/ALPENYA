a6f7ac0b01a044da529eb4750173234338ce76c0
Medellin20 <zola-eric.wendeou@gmail.com>
2026-08-29 13:41:54 +0100

    New Configs

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,7 +3,7 @@ import { Footer } from '@/components/layout/footer';
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="site-background flex min-h-screen flex-col">
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
